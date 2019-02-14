95318d49a513eb62fb1eac1d7695caa6ae138ca7
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-13 17:09:10 -0800

    imported redux thunk and initialized

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {createStore} from 'redux'
-import App from './components/App.jsx'
-import reducers from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from 'redux-thunk'
+import App from "./components/App.jsx";
+import reducers from "./reducers";
 
-const store = createStore(reducers)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector('#root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
