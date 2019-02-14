e2f18403fe48cd98a4a8e9cc2b6887ed301c6d9c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-13 17:09:18 -0800

    added CRUD actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,52 @@
+import streams from "../apis/streams";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_SINGLE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "../actions/types";
+
+// calls an action of SIGN_IN that is passed to our reducer so we can change state, we also take in a userId as a parameter and pass it to our authReducer as the payload.
+export const signIn = userId => {
+  return {
+    type: SIGN_IN,
+    payload: userId
+  };
+};
+
+// calls an action of SIGN_OUT that is passed to our reducer so we can change state.
+export const signOut = () => {
+  return {
+    type: SIGN_OUT
+  };
+};
+
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", {...formValues, userId});
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+  
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchSingleStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_SINGLE_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
