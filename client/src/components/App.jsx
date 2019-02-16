f324088e78460bcd4ffc5d4200a676f7cb60a38e
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-15 17:08:30 -0800

    initialized App component, added dependencies

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route,  } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
 import StreamList from "./streams/StreamList";
 import NavBar from "./NavBar";
+// Importing the createHistory() method from history.js
+import history from '../history'
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+    {
+      // Making our own custom history object only works if we use Router, not BrowserRouter. We then add our createHistory() method which was imported as history as a prop called history
+      // on our Router. BrowserRouter will not accept a custom history object, and has it's own history object.
+    }
+      <Router history={history}>
         <>
           <NavBar />
           <Route path="/" exact component={StreamList} />
@@ -19,7 +25,7 @@ const App = () => {
           <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
         </>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 };
