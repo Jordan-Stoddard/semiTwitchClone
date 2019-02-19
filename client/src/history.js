622bb7a43e310b984d32e539e8f033655cefa353
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-18 17:08:29 -0800

    initializing custom history object

---
+++ client/src/history.js
@@ -0,0 +1,6 @@
+                        // This is a path to the react-browser-router library that references a function called createBrowserHistory
+// We're setting createHistory = createBrowserHistory() essentially 
+import createHistory from 'history/createBrowserHistory'
+
+// we're exporting the method.
+export default createHistory()
