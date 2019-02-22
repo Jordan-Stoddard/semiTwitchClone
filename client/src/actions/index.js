6d5166c7f17747ed3b670565b346a5757392c14e
Jordan Stoddard <jjsincorporated@gmail.com>
2019-02-22 10:53:12 -0800

    added redux-thunk middleware

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from '../history'
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -24,29 +25,42 @@ export const signOut = () => {
   };
 };
 
+// calls an action of CREATE_STREAM
+                  // formValues is the value of the form from StreamCreate
+                                                        // getState allows us to get state that's already been initialized into our store.
 export const createStream = formValues => async (dispatch, getState) => {
+  // We get the userId from the auth object that's already in our redux store's state. (it was passed into our redux store's state when we used signIn)
   const { userId } = getState().auth;
+  // We add a strean object to the streams array on our database and spread in the form values, plus userId: userId with ES6 syntax
   const response = await streams.post("/streams", {...formValues, userId});
+  // dispatch of type CREATE_STREAM, payload is the new stream object with an userId property inside.
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  
+  // Here we're adding a line to our action creator so that when it's called it will automatically re-route our user to a different page.
+  history.push('/')
 };
 
+// get call for all streams in database.
 export const fetchStreams = () => async dispatch => {
   const response = await streams.get("/streams");
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
+// get call for single stream in database selected by the corresponding id property.
 export const fetchSingleStream = id => async dispatch => {
   const response = await streams.get(`/streams/${id}`);
   dispatch({ type: FETCH_SINGLE_STREAM, payload: response.data });
 };
 
+// put call where we select a specific stream object to edit by id, and edit it based on the values that come in from the form.
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/')
 };
 
+// delete call that deletes a specific stream by id.
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push('/')
 };
